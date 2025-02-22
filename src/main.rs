4b0b0cfbb3cc49e93dbebb1bd04a4c0175957f36
Roel Kluin <r.o.e.lkluin@gmail.com>
2025-02-22 20:55:24 +0100

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,7 @@ use tokio::io::{self, AsyncWriteExt};
 use tokio_stream::StreamExt;
 
 use anyhow::{anyhow, Error as AnyError, Result};
-use chrono::{DateTime, FixedOffset};
 use clap::{Parser, ValueEnum};
-use indicatif::ProgressBar;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
@@ -36,7 +34,7 @@ struct Args {
 
     /// Text files to use as input, seperated by commas
     #[clap(short, long)]
-    text_files: String,
+    text_files: Option<String>,
 
     #[clap(short, long, default_value = "http://localhost:11434")]
     server: String,
@@ -45,51 +43,17 @@ struct Args {
     config: Option<String>,
 }
 
-#[derive(Serialize, Deserialize)]
-struct ReqResponsePart {
-    response: String,
-    model: String,
-    created_at: DateTime<FixedOffset>,
-    done: bool,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct ModelDetails {
-    parent_model: String,
-    format: String,
-    family: String,
-    // families: Vec<String>,
-    parameter_size: String,
-    quantization_level: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Model {
-    name: String,
-    model: String,
-    modified_at: DateTime<FixedOffset>,
-    size: u64,
-    digest: String,
-    details: ModelDetails,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct ModelList {
-    models: Vec<Model>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct PullResponsePart {
-    status: String,
-    digest: Option<String>,
-    total: Option<u64>,
-    completed: Option<u64>,
-}
-
 async fn get_model(ollama: &Ollama, name: &str) -> Result<String> {
+    if name.is_empty()
+        || !name
+            .chars()
+            .all(|c| c.is_alphanumeric() || c == ':' || c == '-' || c == '.')
+    {
+        return Err(anyhow::anyhow!("Invalid model name: {}.", name));
+    }
     let model_list = ollama.list_local_models().await?;
     let model = if name.contains(":") {
-        model_list.iter().find(|m| &m.name == name)
+        model_list.iter().find(|m| m.name == name)
     } else {
         model_list.iter().find(|m| m.name.starts_with(name))
     };
@@ -101,29 +65,31 @@ async fn get_model(ollama: &Ollama, name: &str) -> Result<String> {
                 .pull_model(name.to_string(), false)
                 .await
                 .map_err(|e| anyhow!("Failed to pull model: {}", e))?;
-            get_model(ollama, name).await
+            Box::pin(get_model(ollama, name)).await
         }
     }
 }
 
 fn generate_prompt(args: &Args) -> Result<String> {
     let mut prompt = String::new();
-    args.text_files.split(',').try_for_each(|file| {
-        if prompt.is_empty() {
-            prompt.push_str("Considering the input:\n");
-        }
+    if let Some(text_files) = &args.text_files {
+        text_files.split(',').try_for_each(|file| {
+            if prompt.is_empty() {
+                prompt.push_str("Considering the input:\n");
+            }
 
-        if file != "-" {
-            prompt.push_str("file: ");
-            prompt.push_str(file);
-        } else {
-            prompt.push_str("stdin:");
-        }
-        prompt.push_str("\n```\n");
-        prompt.push_str(&std::fs::read_to_string(file)?);
-        prompt.push_str("\n```\n");
-        Ok::<(), AnyError>(())
-    })?;
+            if file != "-" {
+                prompt.push_str("file: ");
+                prompt.push_str(file);
+            } else {
+                prompt.push_str("stdin:");
+            }
+            prompt.push_str("\n```\n");
+            prompt.push_str(&std::fs::read_to_string(file)?);
+            prompt.push_str("\n```\n");
+            Ok::<(), AnyError>(())
+        })?;
+    }
     prompt.push_str(&args.prompt);
     Ok(prompt)
 }
@@ -157,12 +123,6 @@ async fn main() -> Result<()> {
         }
         None => return Err(anyhow::anyhow!("Invalid server address: {}", args.server)),
     };
-    let pb = ProgressBar::new(100);
-    pb.set_style(
-        indicatif::ProgressStyle::default_bar()
-            .template("{msg} {bar:40.cyan/blue} {pos:>7}/{len:7} {percent}%")?
-            .progress_chars("=> "),
-    );
     let completed_model_name = get_model(&ollama, &args.model).await?;
 
     let prompt = generate_prompt(&args)?;
@@ -187,10 +147,8 @@ async fn main() -> Result<()> {
     while let Some(res) = stream.next().await {
         let responses = res?;
         for resp in responses {
-            while let Some(chunk) = resp.response.as_bytes().chunks(1024).next() {
-                stdout.write_all(chunk).await?;
-                stdout.flush().await?;
-            }
+            stdout.write_all(resp.response.as_bytes()).await?;
+            stdout.flush().await?;
         }
     }
     return Ok(());
