f30e8a659ce65211eb6818668511720e35c89b7b
Roel Kluin <r.o.e.lkluin@gmail.com>
2026-01-13 07:43:48 +0100

    zip_up update

--- zip_up.sh
+++ zip_up.sh
@@ -6,6 +6,8 @@ REPO_NAME=$(basename "$(git rev-parse --show-toplevel)")
 ZIP_FILE="${REPO_NAME}.zip"
 echo "Creating zip file: ${ZIP_FILE}"
 git archive -o "${ZIP_FILE}" HEAD
+zip "${ZIP_FILE}" -r .git
+zip "${ZIP_FILE}" docs/multi_agent_system.odt
 echo "Zip file created successfully."
 
 
