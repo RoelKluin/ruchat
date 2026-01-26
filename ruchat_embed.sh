d44166dbd0076beda9e42ab4d38acbedbcd45f13
RoelKluin <roel.kluin@gmail.com>
2026-01-26 07:17:45 +0100

    update

--- ruchat_embed.sh
+++ ruchat_embed.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-find src -name "*.rs" | while read -r f; do
+git ls-files | grep -v '^ruchat$' | while read -r f; do
     id="Contents of file: $f"
     ./ruchat embed -m all-minilm:l6-v2 "$id:\n\`\`\`\n$(cat "$f")\n\`\`\`"
 done
+
+# Embed git commit messages for the src directory
+git log --pretty=oneline -- src | while read -r line; do
+    commit_hash=$(echo "$line" | awk '{print $1}')
+    ./ruchat embed -m all-minilm:l6-v2 "$line:\n$(git log -n 1 -p "$commit_hash")"
+done
+
+# Embed contents of all files in the src directory
+ctags -R src
+f=tags
+id="Contents of file: $f"
+./ruchat embed -m all-minilm:l6-v2 "$id:\n\`\`\`\n$(cat "$f")\n\`\`\`"
+
+
