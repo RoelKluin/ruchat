206a3d2c9cebbd9ad1e85c18584b338c664ce5f0
Roel Kluin <r.o.e.lkluin@gmail.com>
2026-01-30 15:11:56 +0100

    update embed_script

--- embed_script.sh
+++ embed_script.sh
@@ -1,217 +1,51 @@
 #!/bin/bash
 declare -A ext
 
-# A
-ext[".inp"]="Abaqus"
-ext[".abc"]="Abc"
-ext[".adb"]="Ada"
-ext[".ads"]="Ada"
-ext[".yml"]="Yaml"
-ext[".yaml"]="Yaml"
-ext[".yml:AnsiblePlaybook"]="AnsiblePlaybook"
-ext[".yaml:AnsiblePlaybook"]="AnsiblePlaybook"
-ext[".xml:Ant"]="Ant"
-ext[".adoc"]="Asciidoc"
-ext[".asc"]="Asciidoc"
-ext[".asm"]="Asm"
-ext[".asp"]="Asp"
-ext[".au3"]="AutoIt"
-ext[".ac"]="Autoconf"
-ext[".am"]="Automake"
-ext[".awk"]="Awk"
-
-# B
-ext[".beta"]="BETA"
-ext[".bas"]="Basic"
-ext[".bats"]="Bats"
-ext[".bib"]="BibTeX"
-ext[".bib:BibLaTeX"]="BibLaTeX"
-
-# C
-ext[".c"]="C"
-ext[".h"]="C++"
-ext[".cs"]="C#"
-ext[".cpp"]="C++"
-ext[".cxx"]="C++"
-ext[".cmake"]="CMake"
-ext[".css"]="CSS"
-ext[".cu"]="CUDA"
-ext[".toml:Cargo"]="Cargo"
-ext[".clj"]="Clojure"
-ext[".cbl"]="Cobol"
-ext[".cob"]="Cobol"
-ext[".cbl:CobolFree"]="CobolFree"
-ext[".tags"]="Ctags"
-
-# D
-ext[".d"]="D"
-ext[".xml:DBusIntrospect"]="DBusIntrospect"
-ext[".dtd"]="DTD"
-ext[".dts"]="DTS"
-ext[".diff"]="Diff"
-ext[".patch"]="Diff"
-ext[".bat"]="DosBatch"
-
-# E
-ext[".e"]="Eiffel"
-ext[".ex"]="Elixir"
-ext[".elm"]="Elm"
-ext[".el"]="EmacsLisp"
-ext[".erl"]="Erlang"
-
-# F
-ext[".fal"]="Falcon"
-ext[".l"]="LEX"
-ext[".l:Flex"]="Flex"
-ext[".fs"]="Forth"
-ext[".f90"]="Fortran"
-ext[".fypp"]="Fypp"
-
-# G
-ext[".gd"]="GDScript"
-ext[".gperf"]="GPerf"
-ext[".gdbinit"]="Gdbinit"
-ext[".gemspec"]="GemSpec"
-ext[".glade"]="Glade"
-ext[".go"]="Go"
-
-# H
-ext[".html"]="HTML"
-ext[".hs"]="Haskell"
-ext[".hx"]="Haxe"
-
-# I
-ext[".rb:I18nRubyGem"]="I18nRubyGem"
-ext[".ipynb"]="IPythonCell"
-ext[".itcl"]="ITcl"
-ext[".ini"]="Iniconf"
-ext[".inko"]="Inko"
-ext[".jni"]="JNI"
-
-# J
-ext[".json"]="JSON"
-ext[".java"]="Java"
-ext[".properties"]="JavaProperties"
-ext[".js"]="JavaScript"
-ext[".jl"]="Julia"
-
-# K
-ext[".kconfig"]="Kconfig"
-ext[".kt"]="Kotlin"
-
-# L
-ext[".ld"]="LdScript"
-ext[".lisp"]="Lisp"
-ext[".lhs"]="LiterateHaskell"
-ext[".lua"]="Lua"
-
-# M
-ext[".m"]="ObjectiveC"
-ext[".m:MatLab"]="MatLab"
-ext[".m4"]="M4"
-ext[".mk"]="Make"
-ext[".man"]="Man"
-ext[".md"]="Markdown"
-ext[".xml:Maven2"]="Maven2"
-ext[".meson"]="Meson"
-ext[".meson.options"]="MesonOptions"
-ext[".pm:Moose"]="Moose"
-ext[".myr"]="Myrddin"
-
-# N
-ext[".nsi"]="NSIS"
-
-# O
-ext[".ml"]="OCaml"
-ext[".yaml:OpenAPI"]="OpenAPI"
-ext[".org"]="Org"
-
-# P
-ext[".php"]="PHP"
-ext[".pas"]="Pascal"
-ext[".passwd"]="Passwd"
-ext[".pl"]="Perl"
-ext[".pod"]="Pod"
-ext[".ps1"]="PowerShell"
-ext[".proto"]="Protobuf"
-ext[".pp"]="PuppetManifest"
-ext[".py"]="Python"
-ext[".cfg:PythonLoggingConfig"]="PythonLoggingConfig"
-
-# Q
-ext[".moc"]="QtMoc"
-ext[".qmd"]="Quarto"
-
-# R
-ext[".r"]="R"
-ext[".r6"]="R6Class"
-ext[".rd"]="RDoc"
-ext[".rex"]="REXX"
-ext[".rmd"]="RMarkdown"
-ext[".rb"]="Ruby"
-ext[".rs"]="Rust"
-
-# S
-ext[".s4"]="S4Class"
-ext[".scss"]="SCSS"
-ext[".if"]="SELinuxInterface"
-ext[".te"]="SELinuxTypeEnforcement"
-ext[".sl"]="SLang"
-ext[".sml"]="SML"
-ext[".sql"]="SQL"
-ext[".svg"]="SVG"
-ext[".scd"]="Scdoc"
-ext[".scm"]="Scheme"
-ext[".sh"]="Sh"
-ext[".stp"]="SystemTap"
-ext[".sv"]="SystemVerilog"
-ext[".service"]="SystemdUnit"
-
-# T
-ext[".toml"]="TOML"
-ext[".ttcn"]="TTCN"
-ext[".tcl"]="Tcl"
-ext[".tex:TeXBeamer"]="TeXBeamer"
-ext[".tf"]="Terraform"
-ext[".tex"]="Tex"
-ext[".thrift"]="Thrift"
-ext[".t2t"]="Txt2tags"
-ext[".ts"]="TypeScript"
-ext[".tsp"]="TypeSpec"
-
-# V
-ext[".v"]="Verilog"
-ext[".vhdl"]="VHDL"
-ext[".varlink"]="Varlink"
-ext[".vr"]="Vera"
-ext[".vim"]="Vim"
-ext[".rc"]="WindRes"
-
-# X
-ext[".xml"]="XML"
-ext[".xrc"]="XRC"
-ext[".xslt"]="XSLT"
-
-# Y
-ext[".y"]="YACC"
-ext[".repo"]="YumRepo"
-
-# Z
-ext[".zep"]="Zephir"
-ext[".zsh"]="Zsh"
-
-
+while read ext lang; do
+    [[ "${ext:0:1}" == "#" ]] && continue
+    [[ -z "$ext" || -z "$lang" ]] && continue
+    ext["$ext"]="$lang"
+done < <(cat etc/file_extensions.txt)
 
 git ls-files | grep -v '^ruchat$' | xargs ctags
 declare -A tags
 s="[:space:]"
 S="[^$s]"
 s="[$s]"
-#ctags --list-kinds-full | sed -n -r 's~^(Rust|Sh|Markdown|TOML)$s+([a-zA-Z])$s+($S+)($s+$S+){4}$s+(.*)$~\2 \1:\3:\4~p' |
+#ctags --list-kinds-full | sed -n -E 's~^(Rust|Sh|Markdown|TOML)$s+([a-zA-Z])$s+($S+)($s+$S+){4}$s+(.*)$~\2 \1:\3:\4~p' |
 while read -r k v; do
   tags[$k]="$v"
-done < <(ctags --list-kinds-full -R src | sed -n -r "s~^($S+)$s+([a-zA-Z])$s+($S+)($s+$S+){4}$s+(.*)$~\1:\2 \3~p")
+done < <(ctags --list-kinds-full -R src | sed -n -E "s~^($S+)$s+([a-zA-Z])$s+($S+)($s+$S+){4}$s+(.*)$~\1:\2 \3~p")
 
+#Rust:C constant
+#Rust:M macro
+#Rust:P method
+#Rust:c implementation
+#Rust:e enumerator
+#Rust:f function
+#Rust:g enum
+#Rust:i interface
+#Rust:m field
+#Rust:n module
+#Rust:s struct
+#Rust:t typedef
+#Rust:v variable
+#Markdown:S subsection
+#Markdown:T l4subsection
+#Markdown:c chapter
+#Markdown:h hashtag
+#Markdown:n footnote
+#Markdown:s section
+#Markdown:t subsubsection
+#Markdown:u l5subsection
+#Sh:a alias
+#Sh:f function
+#Sh:h heredoc
+#Sh:s script
+#TOML:K qkey
+#TOML:a arraytable
+#TOML:k key
+#TOML:t table
 
 
 model="all-minilm:l6-v2"
@@ -237,15 +71,7 @@ git ls-files | grep -v '^ruchat$' | while read -r f; do
                     kind="${kind%%[[:space:]]*}"
                 }
 
-                # Determine type & clean value
-                if [[ $ex_cmd =~ ^/.*/$ ]]; then
-                    ex_cmd="${ex_cmd:1:-1}"
-                    ex_type="regex"
-                else
-                    ex_type="line"
-                fi
-
-                rust_kind="${tags[$lang:${kind}]:-$kind}"
+                lang_kind="${tags[$lang:${kind}]:-$kind}"
 
                 # Build tags object
                 tags_json="{}"
@@ -259,31 +85,84 @@ git ls-files | grep -v '^ruchat$' | while read -r f; do
                         tags_json=$(jq -n --argjson prev "$tags_json" --arg k "$tkey" --arg v "$tval" '$prev + {($k): $v}')
                     done
                 fi
+                start=""
+                end=""
+                # Determine type & clean value
+                if [[ $ex_cmd =~ ^/.*/$ ]]; then
+                    # XXX: still Uncertain about these: '.?|' certain about excluding '+{}()\\'
+                    ex_cmd="/^$(echo "${ex_cmd:2:-2}" | sed -r 's/([][*^$])/\\\1/g')$/"
+                    start="$(sed -n "${ex_cmd}{=;q}" "$f")"
+                    end="$start"
+                    # FIXME: improver per lang/kind handling here:
+                    if [[ "$lang:$lang_kind" =~ ^Rust:(function|method|implementation|macro|module|struct|enum)$ ]] || \
+                            [[ "$lang:$lang_kind" =~ ^Sh:(function|script|heredoc)$ ]] || \
+                            [[ "$lang:$lang_kind" =~ ^TOML:(arraytable|table|key)$ ]] || \
+                            [[ "$lang:$lang_kind" == "Markdown:(chapter|section|subsection|subsubsection|l4subsection|l5subsection|footnote|hashtag)" ]]; then
+                        # For these kinds, try to find the closing brace
+                        # For these kinds, try to find the closing brace
+                        ex_end_cmd=$(echo "$ex_cmd" | sed -E -n 's~(/\^[ \t]*).*$~\1[^[:space:]].*$/~p')
+                        if [[ -n "$ex_end_cmd" ]]; then
+                            end=$(sed -n "${ex_cmd},${ex_end_cmd}{${ex_cmd}b;${ex_end_cmd}{=;q}}" "$f")
+                            [[ -z "$end" ]] && end="$start"
+                        fi
+                    fi
+                elif [[ $ex_cmd =~ ^[0-9]+$ ]]; then
+                    start="$ex_cmd"
+                    end="$start"
+                    # FIXME: improve per lang/kind handling here:
+                    if [[ "$lang:$lang_kind" =~ ^Rust:(function|method|implementation|macro|module|struct|enum)$ ]] || \
+                            [[ "$lang:$lang_kind" =~ ^Sh:function$ ]]; then
+                        # For these kinds, try to find the closing brace
+                        total_lines=$(wc -l < "$f")
+                        brace_count=0
+                        for (( line_num=start; line_num<=total_lines; line_num++ )); do
+                            line_content=$(sed -n "${line_num}p" "$f")
+                            # Count opening and closing braces
+                            open_braces=$(grep -o '{' <<< "$line_content" | wc -l)
+                            close_braces=$(grep -o '}' <<< "$line_content" | wc -l)
+                            brace_count=$((brace_count + open_braces - close_braces))
+                            if (( brace_count <= 0 )); then
+                                end=$line_num
+                                break
+                            fi
+                        done
+                    fi
+                fi
+                
+                if [[ -z "$start" ]]; then
+                    echo "Warning: Could not determine start line for $var in $f with ex_cmd: $ex_cmd" >&2
+                    continue
+                fi
 
                 # Build one metadata object per item
-                item_json=$(jq -n \
+                item_json=$(if ! jq -n \
                     --arg file    "$f" \
                     --arg lang    "$lang" \
-                    --arg kind    "$rust_kind" \
+                    --arg kind    "$lang_kind" \
                     --arg name    "$var" \
-                    --arg type    "$ex_type" \
-                    --arg value   "$ex_cmd" \
+                    --arg start   "$start" \
+                    --arg end     "$end" \
                     --argjson tags "$tags_json" \
                     '{
                         file:     $file,
                         language: $lang,
                         kind:     $kind,
                         name:     $name,
-                        type:     $type,
-                        value:    $value,
+                        start:    ($start | tonumber),
+                        end:      ($end | tonumber),
                         tags:     $tags
-                    }')
+                    }'; then
+                    echo "Error building item JSON for $var in $f" >&2
+                fi)
+                [ -z "$item_json" ] && continue
 
                 # Append to array
-                metadata_json=$(jq -n --argjson arr "$metadata_json" --argjson item "$item_json" '$arr + [$item]')
+                metadata_json=$(if ! jq -n --argjson arr "$metadata_json" --argjson item "$item_json" '$arr + [$item]'; then
+                    echo "Error appending item JSON for $var in $f" >&2
+                fi)
+                [ -z "$metadata_json" ] && continue
 
-            done < <(sed -n -E "s/^($S+)\t${f}\t((\/.*\\$\/|[0-9]+)+)(;\"((\t$S+)*))?$/\1\r\2\r\5/p" tags)
-
+            done < <(sed -n -E "s~^($S+)\t$f\t((/.*\\\$/|[0-9]+)+)(;\"((\t$S+)*))?$~\1\r\2\r\5~p" tags)
         fi
     done
 
@@ -292,7 +171,6 @@ git ls-files | grep -v '^ruchat$' | while read -r f; do
 
     if jq -e 'length > 0' <<< "$metadata_json" >/dev/null; then
         embed_args+=("--metadata" "$(jq -c . <<< "$metadata_json")")
-
     else
         echo "No metadata extracted for $f (lang: $lang)" >&2
     fi
