2ddbf5c7190459f1d7ba59e8c518623c5c48a437
Roel Kluin <r.o.e.lkluin@gmail.com>
2026-03-24 21:23:39 +0100

    update working.sh

--- working.sh
+++ working.sh
@@ -1,8 +1,8 @@
 
-./ruchat similarity --model "all-minilm:l6-v2" --query "how can we embed an answer from ask.rs?"
+#./ruchat similarity --model "all-minilm:l6-v2" --query "how can we embed an answer from ask.rs?"
 
-./ruchat chroma-metadata -c repo_src-all-minilm_l6-v2 --where-metadata '{"file": "src/main.rs"}'
-./ruchat chroma-metadata -c repo_src-all-minilm_l6-v2 --where-metadata '{"language": "Rust"}'
+#./ruchat chroma-metadata -c repo_src-all-minilm_l6-v2 --where-metadata '{"file": "src/main.rs"}'
+#./ruchat chroma-metadata -c repo_src-all-minilm_l6-v2 --where-metadata '{"language": "Rust"}'
 
 ./ruchat chroma-query --model 'qwen3:latest' \
 --model "all-minilm:l6-v2" \
@@ -10,7 +10,7 @@
 --query "Contents of file: src/ollama/ask.rs"
 
 
-./ruchat get --model "qwen2.5vl:latest" --collection repo_src-all-minilm_l6-v2  --prompt 'what argument(s) does the ask function in Ask require?'
+./ruchat chroma-get --model "qwen2.5vl:latest" --collection repo_src-all-minilm_l6-v2  --prompt 'what argument(s) does the ask function in Ask require?'
 
 chroma browse repo_src-all-minilm_l6-v2 --host http://localhost:8000
 chroma browse repo_hist-all-minilm_l6-v2 --host http://localhost:8000
